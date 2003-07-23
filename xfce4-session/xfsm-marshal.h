880902cf5c05348535db0a30095350d80ecfb43b
Benedikt Meurer <benny@xfce.org>
2003-07-23 16:00:30 +0000

    Added initial marshallers stuff.
    (Old svn revision: 4470)

---
+++ xfce4-session/xfsm-marshal.h
@@ -0,0 +1,20 @@
+
+#ifndef ___xfsm_marshal_MARSHAL_H__
+#define ___xfsm_marshal_MARSHAL_H__
+
+#include	<glib-object.h>
+
+G_BEGIN_DECLS
+
+/* BOOLEAN:POINTER,POINTER (xfsm-marshal.list:25) */
+extern void _xfsm_marshal_BOOLEAN__POINTER_POINTER (GClosure     *closure,
+                                                    GValue       *return_value,
+                                                    guint         n_param_values,
+                                                    const GValue *param_values,
+                                                    gpointer      invocation_hint,
+                                                    gpointer      marshal_data);
+
+G_END_DECLS
+
+#endif /* ___xfsm_marshal_MARSHAL_H__ */
+
