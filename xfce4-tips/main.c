41022b3180784b4169107ba3f45a54b5466b612b
Jérôme Guelfucci <jeromeg@xfce.org>
2010-02-08 22:59:03 +0100

    Use accessor instead of struct member.

--- xfce4-tips/main.c
+++ xfce4-tips/main.c
@@ -281,7 +281,7 @@ main (int argc, char **argv)
 
   vbox2 = gtk_vbox_new (FALSE, 6);
   gtk_container_set_border_width (GTK_CONTAINER (vbox2), 6);
-  gtk_box_pack_start (GTK_BOX (GTK_DIALOG (dlg)->vbox), vbox2, TRUE, TRUE, 0);
+  gtk_box_pack_start (GTK_BOX (gtk_dialog_get_content_area (GTK_DIALOG (dlg))), vbox2, TRUE, TRUE, 0);
   gtk_widget_show (vbox2);
 
   sw = gtk_scrolled_window_new (NULL, NULL);
