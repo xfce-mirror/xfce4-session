c69e9dcac36581e92a347190c1614f070f086447
DarkTrick <notebook22312@gmail.com>
2020-06-23 15:09:11 +0200

    settings: Add sort-on-click to columns in "Current Session" tab (!3)
    GTK bugs for sorting wrongly still exist here (https://gitlab.gnome.org/GNOME/gtk/-/issues/1848)

--- settings/session-editor.c
+++ settings/session-editor.c
@@ -769,6 +769,8 @@ session_editor_populate_treeview(GtkTreeView *treeview)
                                                    "text", COL_PRIORITY,
                                                    NULL);
     gtk_tree_view_append_column(treeview, col);
+    gtk_tree_view_column_set_sort_column_id(col,COL_PRIORITY);
+
     g_signal_connect(render, "edited",
                      G_CALLBACK(priority_changed), treeview);
 
@@ -777,11 +779,13 @@ session_editor_populate_treeview(GtkTreeView *treeview)
                                                    "text", COL_PID,
                                                    NULL);
     gtk_tree_view_append_column(treeview, col);
+    gtk_tree_view_column_set_sort_column_id(col,COL_PID);
 
     col = gtk_tree_view_column_new();
     gtk_tree_view_column_set_title(col, _("Program"));
     g_object_set(col, "expand", TRUE, NULL);
     gtk_tree_view_append_column(treeview, col);
+    gtk_tree_view_column_set_sort_column_id(col,COL_NAME);
 
     render = gtk_cell_renderer_pixbuf_new();
     gtk_tree_view_column_pack_start(col, render, FALSE);
@@ -808,6 +812,8 @@ session_editor_populate_treeview(GtkTreeView *treeview)
                                                    "text", COL_RESTART_STYLE_STR,
                                                    NULL);
     gtk_tree_view_append_column(treeview, col);
+    gtk_tree_view_column_set_sort_column_id(col,COL_RESTART_STYLE_STR);
+
     g_object_unref(combo_model);
     g_signal_connect(render, "edited",
                      G_CALLBACK(restart_style_hint_changed), treeview);
