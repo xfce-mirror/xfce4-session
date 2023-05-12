xfce-mirror/xfce4-session

commit fac0b30e93f1b6d4d4590851c78347bfbfad0fd8

cleanup: Move common locker code in xfsm-shutdown.c
The resulting code is not strictly equivalent to the original since the
xfsm_consolekit_can_*() checks before locking are removed, but they seem
superfluous:
* if two separate methods try() and can() exist it is not so that can()
  is systematically called in try();
* this call does not take place in xfsm-systemd.c;
* other calls of can() methods take place as they should upstream in the
  code.

xfce4-session/xfsm-shutdown.c
@@ -89,7 +89,8 @@ struct _XfsmShutdown
   XfsmSystemd    *systemd;
   XfsmConsolekit *consolekit;
 
-  XfsmInhibitor  *inhibitions;
+  XfsmInhibitor   *inhibitions;
+  XfceScreensaver *screensaver;
 
   /* kiosk settings */
   gboolean        kiosk_can_shutdown;
@@ -127,6 +128,7 @@ xfsm_shutdown_init (XfsmShutdown *shutdown)
     shutdown->consolekit = xfsm_consolekit_get ();
 
   shutdown->inhibitions = xfsm_inhibitor_get ();
+  shutdown->screensaver = xfce_screensaver_new ();
 
   /* check kiosk */
   kiosk = xfce_kiosk_new ("xfce4-session");
@@ -150,6 +152,8 @@ xfsm_shutdown_finalize (GObject *object)
 
   if (shutdown->inhibitions != NULL)
     g_object_unref (G_OBJECT (shutdown->inhibitions));
+
+  g_object_unref (G_OBJECT (shutdown->screensaver));
 
   (*G_OBJECT_CLASS (xfsm_shutdown_parent_class)->finalize) (object);
 }
@@ -301,11 +305,29 @@ try_sleep_method (gpointer  object,
 
 
 
+static gboolean
+lock_screen (XfsmShutdown *shutdown)
+{
+  XfconfChannel *channel;
+  gboolean ret = TRUE;
+
+  channel = xfconf_channel_get (SETTINGS_CHANNEL);
+  if (xfconf_channel_get_bool (channel, "/shutdown/LockScreen", FALSE))
+    ret = xfce_screensaver_lock (shutdown->screensaver);
+
+  return ret;
+}
+
+
+
 gboolean
 xfsm_shutdown_try_suspend (XfsmShutdown  *shutdown,
                            GError       **error)
 {
   g_return_val_if_fail (XFSM_IS_SHUTDOWN (shutdown), FALSE);
+
+  if (!lock_screen (shutdown))
+    return FALSE;
 
   /* Try each way to suspend - it will handle NULL.
    */
@@ -327,6 +349,9 @@ xfsm_shutdown_try_hibernate (XfsmShutdown  *shutdown,
 {
   g_return_val_if_fail (XFSM_IS_SHUTDOWN (shutdown), FALSE);
 
+  if (!lock_screen (shutdown))
+    return FALSE;
+
   /* Try each way to hibernate - it will handle NULL.
    */
 
@@ -346,6 +371,9 @@ xfsm_shutdown_try_hybrid_sleep (XfsmShutdown  *shutdown,
                                 GError       **error)
 {
   g_return_val_if_fail (XFSM_IS_SHUTDOWN (shutdown), FALSE);
+
+  if (!lock_screen (shutdown))
+    return FALSE;
 
   /* Try each way to hybrid-sleep - it will handle NULL.
    */

xfce4-session/xfsm-consolekit.c
@@ -48,7 +48,6 @@ struct _XfsmConsolekit
 
   GDBusProxy *proxy;
   guint name_id;
-  XfceScreensaver *screensaver;
 };
 
 
@@ -129,8 +128,6 @@ xfsm_consolekit_init (XfsmConsolekit *consolekit)
                                           name_lost,
                                           consolekit,
                                           NULL);
-
-  consolekit->screensaver = xfce_screensaver_new ();
 }
 
 
@@ -152,12 +149,6 @@ xfsm_consolekit_proxy_free (XfsmConsolekit *consolekit)
     {
       g_object_unref (G_OBJECT (consolekit->proxy));
       consolekit->proxy = NULL;
-    }
-
-  if (consolekit->screensaver != NULL)
-    {
-      g_object_unref (G_OBJECT (consolekit->screensaver));
-      consolekit->screensaver = NULL;
     }
 }
 
@@ -386,41 +377,12 @@ xfsm_consolekit_can_shutdown (XfsmConsolekit  *consolekit,
 }
 
 
-static gboolean
-lock_screen (XfsmConsolekit  *consolekit,
-             GError **error)
-{
-  XfconfChannel *channel;
-  gboolean       ret = TRUE;
-
-  channel = xfconf_channel_get (SETTINGS_CHANNEL);
-  if (xfconf_channel_get_bool (channel, "/shutdown/LockScreen", FALSE))
-      ret = xfce_screensaver_lock (consolekit->screensaver);
-
-  return ret;
-}
 
 gboolean
 xfsm_consolekit_try_suspend (XfsmConsolekit  *consolekit,
                              GError         **error)
 {
-  gboolean can_suspend, auth_suspend;
-
   g_return_val_if_fail (XFSM_IS_CONSOLEKIT (consolekit), FALSE);
-
-  /* Check if consolekit can suspend before we call lock screen. */
-  if (xfsm_consolekit_can_suspend (consolekit, &can_suspend, &auth_suspend, NULL))
-    {
-      if (!can_suspend)
-        return FALSE;
-    }
-  else
-    {
-      return FALSE;
-    }
-
-  if (!lock_screen (consolekit, error))
-    return FALSE;
 
   return xfsm_consolekit_try_sleep (consolekit, "Suspend", error);
 }
@@ -431,23 +393,7 @@ gboolean
 xfsm_consolekit_try_hibernate (XfsmConsolekit  *consolekit,
                                GError         **error)
 {
-  gboolean can_hibernate, auth_hibernate;
-
   g_return_val_if_fail (XFSM_IS_CONSOLEKIT (consolekit), FALSE);
-
-  /* Check if consolekit can hibernate before we call lock screen. */
-  if (xfsm_consolekit_can_hibernate (consolekit, &can_hibernate, &auth_hibernate, NULL))
-    {
-      if (!can_hibernate)
-        return FALSE;
-    }
-  else
-    {
-      return FALSE;
-    }
-
-  if (!lock_screen (consolekit, error))
-    return FALSE;
 
   return xfsm_consolekit_try_sleep (consolekit, "Hibernate", error);
 }
@@ -458,23 +404,7 @@ gboolean
 xfsm_consolekit_try_hybrid_sleep (XfsmConsolekit  *consolekit,
                                   GError         **error)
 {
-  gboolean can_hybrid_sleep, auth_hybrid_sleep;
-
   g_return_val_if_fail (XFSM_IS_CONSOLEKIT (consolekit), FALSE);
-
-  /* Check if consolekit can hybrid sleep before we call lock screen. */
-  if (xfsm_consolekit_can_hybrid_sleep (consolekit, &can_hybrid_sleep, &auth_hybrid_sleep, NULL))
-    {
-      if (!can_hybrid_sleep)
-        return FALSE;
-    }
-  else
-    {
-      return FALSE;
-    }
-
-  if (!lock_screen (consolekit, error))
-    return FALSE;
 
   return xfsm_consolekit_try_sleep (consolekit, "HybridSleep", error);
 }

xfce4-session/xfsm-systemd.c
@@ -58,7 +58,6 @@ struct _XfsmSystemd
   GObject __parent__;
 
   XfsmPackagekit  *packagekit;
-  XfceScreensaver *screensaver;
 };
 
 
@@ -82,7 +81,6 @@ static void
 xfsm_systemd_init (XfsmSystemd *systemd)
 {
   systemd->packagekit = xfsm_packagekit_get ();
-  systemd->screensaver = xfce_screensaver_new ();
 }
 
 
@@ -93,25 +91,8 @@ xfsm_systemd_finalize (GObject *object)
   XfsmSystemd *systemd = XFSM_SYSTEMD (object);
 
   g_object_unref (G_OBJECT (systemd->packagekit));
-  g_object_unref (G_OBJECT (systemd->screensaver));
 
   (*G_OBJECT_CLASS (xfsm_systemd_parent_class)->finalize) (object);
-}
-
-
-
-static gboolean
-xfsm_systemd_lock_screen (XfsmSystemd  *systemd,
-                          GError **error)
-{
-  XfconfChannel *channel;
-  gboolean       ret = TRUE;
-
-  channel = xfconf_channel_get (SETTINGS_CHANNEL);
-  if (xfconf_channel_get_bool (channel, "/shutdown/LockScreen", FALSE))
-      ret = xfce_screensaver_lock (systemd->screensaver);
-
-  return ret;
 }
 
 
@@ -256,9 +237,6 @@ gboolean
 xfsm_systemd_try_suspend (XfsmSystemd  *systemd,
                           GError      **error)
 {
-  if (!xfsm_systemd_lock_screen (systemd, error))
-    return FALSE;
-
   return xfsm_systemd_try_method (systemd,
                                   SYSTEMD_SUSPEND_ACTION,
                                   error);
@@ -270,9 +248,6 @@ gboolean
 xfsm_systemd_try_hibernate (XfsmSystemd  *systemd,
                             GError      **error)
 {
-  if (!xfsm_systemd_lock_screen (systemd, error))
-    return FALSE;
-
   return xfsm_systemd_try_method (systemd,
                                   SYSTEMD_HIBERNATE_ACTION,
                                   error);
@@ -284,9 +259,6 @@ gboolean
 xfsm_systemd_try_hybrid_sleep (XfsmSystemd  *systemd,
                                GError      **error)
 {
-  if (!xfsm_systemd_lock_screen (systemd, error))
-    return FALSE;
-
   return xfsm_systemd_try_method (systemd,
                                   SYSTEMD_HYBRID_SLEEP_ACTION,
                                   error);

commit ea39ae14ebb310f379591af4091c261a2681c613

shutdown: Properly set error when methods fail
Avoids a crash when accessing error in xfsm-manager.c:1274. Also put
and share locking code above all shutdown methods, without waiting for
lock command to execute since it is now executed synchronously.

Fixes: fac0b30e93f1b6d4d4590851c78347bfbfad0fd8
Related: !39, libxfce4ui@32f761266658e2f59093af259655e44efecf31a9

xfce4-session/xfsm-shutdown-fallback.c
@@ -329,33 +329,6 @@ xfsm_shutdown_fallback_check_auth_polkit (const gchar *action_id)
 
 
 
-static gboolean
-lock_screen (GError **error)
-{
-  XfconfChannel *channel;
-  XfceScreensaver *saver;
-  gboolean       ret = TRUE;
-
-  channel = xfconf_channel_get (SETTINGS_CHANNEL);
-  saver = xfce_screensaver_new ();
-  if (xfconf_channel_get_bool (channel, "/shutdown/LockScreen", FALSE))
-      ret = xfce_screensaver_lock (saver);
-
-  if (ret)
-    {
-      /* sleep 2 seconds so locking has time to startup */
-      g_usleep (G_USEC_PER_SEC * 2);
-    }
-  else
-    g_set_error (error, 1, 0, "Failed to lock the screen.");
-
-  g_object_unref (G_OBJECT (saver));
-
-  return ret;
-}
-
-
-
 /**
  * xfsm_shutdown_fallback_try_action:
  * @type:  The @XfsmShutdownType action to perform (shutdown, suspend, etc).
@@ -392,23 +365,14 @@ xfsm_shutdown_fallback_try_action (XfsmShutdownType   type,
     case XFSM_SHUTDOWN_SUSPEND:
       xfsm_helper_action = "suspend";
       cmd = UP_BACKEND_SUSPEND_COMMAND;
-      /* On suspend we try to lock the screen */
-      if (!lock_screen (error))
-        return FALSE;
       break;
     case XFSM_SHUTDOWN_HIBERNATE:
       xfsm_helper_action = "hibernate";
       cmd = UP_BACKEND_HIBERNATE_COMMAND;
-      /* On hibernate we try to lock the screen */
-      if (!lock_screen (error))
-        return FALSE;
       break;
     case XFSM_SHUTDOWN_HYBRID_SLEEP:
       xfsm_helper_action = "hybrid-sleep";
       cmd = UP_BACKEND_HIBERNATE_COMMAND;
-      /* On hybrid sleep we try to lock the screen */
-      if (!lock_screen (error))
-        return FALSE;
       break;
     default:
       g_set_error (error, 1, 0, "Unknown shutdown type %d", type);

xfce4-session/xfsm-shutdown.c
@@ -329,7 +329,8 @@ try_sleep_method (gpointer  object,
 
 
 static gboolean
-lock_screen (XfsmShutdown *shutdown)
+lock_screen (XfsmShutdown *shutdown,
+             GError **error)
 {
   XfconfChannel *channel;
   gboolean ret = TRUE;
@@ -337,6 +338,9 @@ lock_screen (XfsmShutdown *shutdown)
   channel = xfconf_channel_get (SETTINGS_CHANNEL);
   if (xfconf_channel_get_bool (channel, "/shutdown/LockScreen", FALSE))
     ret = xfce_screensaver_lock (shutdown->screensaver);
+
+  if (!ret)
+    g_set_error (error, 1, 0, "Failed to lock the screen.");
 
   return ret;
 }
@@ -349,7 +353,7 @@ xfsm_shutdown_try_suspend (XfsmShutdown  *shutdown,
 {
   g_return_val_if_fail (XFSM_IS_SHUTDOWN (shutdown), FALSE);
 
-  if (!lock_screen (shutdown))
+  if (!lock_screen (shutdown, error))
     return FALSE;
 
   /* Try each way to suspend - it will handle NULL.
@@ -372,7 +376,7 @@ xfsm_shutdown_try_hibernate (XfsmShutdown  *shutdown,
 {
   g_return_val_if_fail (XFSM_IS_SHUTDOWN (shutdown), FALSE);
 
-  if (!lock_screen (shutdown))
+  if (!lock_screen (shutdown, error))
     return FALSE;
 
   /* Try each way to hibernate - it will handle NULL.
@@ -395,7 +399,7 @@ xfsm_shutdown_try_hybrid_sleep (XfsmShutdown  *shutdown,
 {
   g_return_val_if_fail (XFSM_IS_SHUTDOWN (shutdown), FALSE);
 
-  if (!lock_screen (shutdown))
+  if (!lock_screen (shutdown, error))
     return FALSE;
 
   /* Try each way to hybrid-sleep - it will handle NULL.