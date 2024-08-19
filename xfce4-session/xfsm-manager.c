xfce-mirror/xfce4-session

commit d110f132d8fa861297a4d8a2a0e9a0a4f695009b

Fix blurry session snapshots

xfce4-session/xfsm-manager.c
@@ -428,13 +428,24 @@ xfsm_manager_choose_session (XfsmManager *manager,
                              XfceRc      *rc)
 {
   XfsmSessionInfo *session;
+  GdkDisplay      *display;
+  GdkMonitor      *monitor;
   gboolean         load = FALSE;
   GList           *sessions = NULL;
   GList           *lp;
   gchar           *name;
   gint             result;
+  gint             monitor_num = 0;
+  gint             scale_factor;
 
-  sessions = settings_list_sessions (rc);
+  display = gdk_display_get_default ();
+  xfce_gdk_screen_get_active (&monitor_num);
+  monitor = gdk_display_get_monitor (display, monitor_num);
+  if (G_UNLIKELY (monitor == NULL))
+    monitor = gdk_display_get_primary_monitor (display);
+  scale_factor = gdk_monitor_get_scale_factor (monitor);
+
+  sessions = settings_list_sessions (rc, scale_factor);
 
   if (sessions != NULL)
     {

libxfsm/xfsm-util.c
@@ -36,6 +36,8 @@
 
 #include <X11/Xlib.h>
 
+#include <cairo-gobject.h>
+
 #include <gdk/gdkx.h>
 
 #include <libxfce4ui/libxfce4ui.h>
@@ -202,11 +204,13 @@ xfsm_gdk_display_get_fullname (GdkDisplay *display)
   return g_strdup (buffer);
 }
 
-GdkPixbuf *
-xfsm_load_session_preview (const gchar *name)
+cairo_surface_t *
+xfsm_load_session_preview (const gchar *name,
+                           gint         size,
+                           gint         scale_factor)
 {
-  GdkDisplay *display;
-  GdkPixbuf  *pb = NULL;
+  cairo_surface_t *icon = NULL;
+  GdkDisplay      *display;
   gchar *display_name;
   gchar *filename;
   gchar *path;
@@ -220,10 +224,17 @@ xfsm_load_session_preview (const gchar *name)
   g_free (path);
 
   if (filename != NULL)
-    pb = gdk_pixbuf_new_from_file (filename, NULL);
+    {
+      GdkPixbuf *pb = gdk_pixbuf_new_from_file_at_scale (filename, size * scale_factor, size * scale_factor, TRUE, NULL);
+      if (G_LIKELY (pb != NULL))
+        {
+          icon = gdk_cairo_surface_create_from_pixbuf (pb, scale_factor, NULL);
+          g_object_unref (pb);
+        }
+    }
   g_free (filename);
 
-  return pb;
+  return icon;
 }
 
 XfceRc *
@@ -256,10 +267,11 @@ settings_list_sessions_open_rc (void)
 }
 
 GList *
-settings_list_sessions (XfceRc *rc)
+settings_list_sessions (XfceRc *rc,
+                        gint scale_factor)
 {
   XfsmSessionInfo *session;
-  GdkPixbuf       *preview_default = NULL;
+  cairo_surface_t *preview_default = NULL;
   GList           *sessions = NULL;
   gchar          **groups;
   gint             n;
@@ -273,18 +285,26 @@ settings_list_sessions (XfceRc *rc)
           session = g_new0 (XfsmSessionInfo, 1);
           session->name = g_strdup (groups[n] + 9);
           session->atime = xfce_rc_read_int_entry (rc, "LastAccess", 0);
-          session->preview = xfsm_load_session_preview (session->name);
+          session->preview = xfsm_load_session_preview (session->name, 64, scale_factor);
 
           if (session->preview == NULL)
             {
               if (G_UNLIKELY (preview_default == NULL))
                 {
-                  preview_default = gtk_icon_theme_load_icon (gtk_icon_theme_get_default (),
-                                                              "xfce4-logo", 64,
-                                                              GTK_ICON_LOOKUP_GENERIC_FALLBACK, NULL);
+                  GdkPixbuf *pb = gtk_icon_theme_load_icon_for_scale (gtk_icon_theme_get_default (),
+                                                                      "xfce4-logo",
+                                                                      64,
+                                                                      scale_factor,
+                                                                      GTK_ICON_LOOKUP_GENERIC_FALLBACK | GTK_ICON_LOOKUP_FORCE_SIZE,
+                                                                      NULL);
+                  if (G_LIKELY (pb != NULL))
+                    {
+                      preview_default = gdk_cairo_surface_create_from_pixbuf (pb, scale_factor, NULL);
+                      g_object_unref (pb);
+                    }
                 }
 
-              session->preview = GDK_PIXBUF (g_object_ref (preview_default));
+              session->preview = cairo_surface_reference (preview_default);
             }
 
           sessions = g_list_append (sessions, session);
@@ -292,7 +312,7 @@ settings_list_sessions (XfceRc *rc)
     }
 
   if (preview_default != NULL)
-    g_object_unref (preview_default);
+    cairo_surface_destroy (preview_default);
 
   g_strfreev (groups);
 
@@ -308,7 +328,7 @@ settings_list_sessions_treeview_init (GtkTreeView *treeview)
   GtkListStore *model;
 
   model = gtk_list_store_new (N_COLUMNS,
-                              GDK_TYPE_PIXBUF,
+                              CAIRO_GOBJECT_TYPE_SURFACE,
                               G_TYPE_STRING,
                               G_TYPE_STRING,
                               G_TYPE_STRING,
@@ -322,7 +342,7 @@ settings_list_sessions_treeview_init (GtkTreeView *treeview)
   renderer = gtk_cell_renderer_pixbuf_new ();
   gtk_tree_view_column_pack_start (column, renderer, FALSE);
   gtk_tree_view_column_set_attributes (column, renderer,
-                                       "pixbuf", PREVIEW_COLUMN,
+                                       "surface", PREVIEW_COLUMN,
                                        NULL);
   gtk_tree_view_append_column (GTK_TREE_VIEW (treeview), column);
 

libxfsm/xfsm-util.h
@@ -37,9 +37,9 @@ typedef struct _XfsmSessionInfo  XfsmSessionInfo;
 
 struct _XfsmSessionInfo
 {
-  gchar     *name;      /* name of the session */
-  time_t     atime;     /* last access time */
-  GdkPixbuf *preview;   /* preview icon (52x42) */
+  gchar           *name;      /* name of the session */
+  time_t           atime;     /* last access time */
+  cairo_surface_t *preview;   /* preview icon (52x42) */
 };
 
 enum
@@ -72,11 +72,14 @@ XfconfChannel *xfsm_open_config                      (void);
 
 gchar         *xfsm_gdk_display_get_fullname         (GdkDisplay *display);
 
-GdkPixbuf     *xfsm_load_session_preview             (const gchar *name);
+cairo_surface_t *xfsm_load_session_preview           (const gchar *name,
+                                                      gint         size,
+                                                      gint         scale_factor);
 
 XfceRc        *settings_list_sessions_open_rc        (void);
 
-GList         *settings_list_sessions                (XfceRc *rc);
+GList         *settings_list_sessions                (XfceRc *rc,
+                                                      gint scale_factor);
 
 void           settings_list_sessions_treeview_init  (GtkTreeView *treeview);
 

settings/main.c
@@ -66,7 +66,7 @@ xfce4_session_settings_show_saved_sessions (GtkBuilder *builder,
         return;
 
     settings_list_sessions_treeview_init (GTK_TREE_VIEW (sessions_treeview));
-    sessions = settings_list_sessions (rc);
+    sessions = settings_list_sessions (rc, gtk_widget_get_scale_factor (sessions_treeview));
     model = gtk_tree_view_get_model (GTK_TREE_VIEW (sessions_treeview));
     settings_list_sessions_populate (model, sessions);
 }

settings/session-editor.c
@@ -164,7 +164,7 @@ session_editor_save_session(GtkWidget *btn,
     if (rc)
     {
         gtk_widget_show (gtk_notebook_get_nth_page (GTK_NOTEBOOK (notebook), 3));
-        sessions = settings_list_sessions (rc);
+        sessions = settings_list_sessions (rc, gtk_widget_get_scale_factor (treeview));
         model = gtk_tree_view_get_model (GTK_TREE_VIEW (treeview));
         /* If the treeview hasn't been initialized we do it now */
         if (!GTK_IS_LIST_STORE (model))

xfce4-session/xfsm-logout-dialog.c
@@ -62,7 +62,7 @@
 
 
 #define BORDER   6
-#define SHOTSIZE 64
+#define SHOTSIZE 256
 
 
 

commit e8da3117e0ba0ffe4c10ac19e3659f791f0f68ed

manager: Fix wrong release function
Fixes: d110f132d8fa861297a4d8a2a0e9a0a4f695009b

xfce4-session/xfsm-manager.c
@@ -473,7 +473,7 @@ xfsm_manager_choose_session (XfsmManager *manager,
       for (lp = sessions; lp != NULL; lp = lp->next)
         {
           session = (XfsmSessionInfo *) lp->data;
-          g_object_unref (session->preview);
+          cairo_surface_destroy (session->preview);
           g_free (session);
         }
 