76f93bba28a9a673c539f34910ea17b30299700d
Gaël Bonithon <gael@xfce.org>
2025-03-21 08:00:24 +0100

    build: Automate copyright year management
    (cherry picked from commit ed60a5b04ace2b7bac1caec6ff4108f18c0c33cd)

--- xfce4-session-logout/main.c
+++ xfce4-session-logout/main.c
@@ -117,7 +117,7 @@ main (int argc, char **argv)
   if (opt_version)
     {
       g_print ("%s %s (Xfce %s)\n\n", PACKAGE_NAME, PACKAGE_VERSION, xfce_version_string ());
-      g_print ("%s\n", "Copyright (c) 2004-2024");
+      g_print ("%s\n", "Copyright (c) 2004-" COPYRIGHT_YEAR);
       g_print ("\t%s\n\n", _("The Xfce development team. All rights reserved."));
       g_print ("%s\n", _("Written by Benedikt Meurer <benny@xfce.org>"));
       g_print ("%s\n\n", _("and Brian Tarricone <kelnos@xfce.org>."));
