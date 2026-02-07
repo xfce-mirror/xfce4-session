64bd4283c98fdc740987e9b9d08344a4d77c8228
Gaël Bonithon <gael@xfce.org>
2026-02-07 05:53:19 +0100

    logout-dialog: Do not create multiple dialogs
    Fixes: #244

--- xfce4-session/xfsm-logout-dialog.c
+++ xfce4-session/xfsm-logout-dialog.c
@@ -687,6 +687,12 @@ xfsm_logout_dialog (const gchar *session_name,
       return TRUE;
     }
 
+  /* do not create multiple dialogs */
+  static gboolean showing_dialog = FALSE;
+  if (showing_dialog)
+    return FALSE;
+  showing_dialog = TRUE;
+
   /* decide on which screen we should show the dialog */
   screen = xfce_gdk_screen_get_active (&monitor);
   if (G_UNLIKELY (screen == NULL))
@@ -797,5 +803,6 @@ xfsm_logout_dialog (const gchar *session_name,
       g_object_unref (G_OBJECT (screenshot));
     }
 
+  showing_dialog = FALSE;
   return (result == GTK_RESPONSE_OK);
 }
